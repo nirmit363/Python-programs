6847a28df452f279eb0a1774a97bd4aa733a9110
Nirmit Rath <nirmit363@gmail.com>
2021-06-08 18:10:33 +0530

    Add files via upload

---
+++ 13_pr4_marks.py
@@ -0,0 +1,11 @@
+m1 = int(input("Enter mark for student numbers 1: "))
+m2 = int(input("Enter mark for student numbers 2: "))
+m3 = int(input("Enter mark for student numbers 3: "))
+m4 = int(input("Enter mark for student numbers 4: "))
+m5 = int(input("Enter mark for student numbers 5: "))
+m6 = int(input("Enter mark for student numbers 6: "))
+
+mylist = [m1, m2, m3, m4, m5, m6]
+mylist.sort()
+
+print (mylist)

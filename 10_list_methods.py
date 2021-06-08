a9cc033f5d7aa3c837726538dd9eaaad3b608477
Nirmit Rath <nirmit363@gmail.com>
2021-06-08 18:08:21 +0530

    Add files via upload

---
+++ 10_list_methods.py
@@ -0,0 +1,12 @@
+list = [1, 2, 4, 5, 6, 9, 7, 11 ,10, 20, 15]
+print (list)
+list.insert(5, 245)
+print (list)
+list.append(45)
+print (list)
+list.sort()
+print (list)
+list.reverse()
+print (list)
+list.remove(11)
+print (list)

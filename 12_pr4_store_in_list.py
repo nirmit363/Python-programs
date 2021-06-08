c9969aaca8e7953e1dd103fd67dbd6b7d1c47f30
Nirmit Rath <nirmit363@gmail.com>
2021-06-08 18:08:43 +0530

    Add files via upload

---
+++ 12_pr4_store_in_list.py
@@ -0,0 +1,10 @@
+f1 = input("Enter the fruit numbers 1 ")
+f2 = input("Enter the fruit numbers 2 ")
+f3 = input("Enter the fruit numbers 3 ")
+f4 = input("Enter the fruit numbers 4 ")
+f5 = input("Enter the fruit numbers 5 ")
+f6 = input("Enter the fruit numbers 6 ")
+f7 = input("Enter the fruit numbers 7 ")
+
+fruitLIst = [f1, f2, f3, f4, f5, f6, f7]
+print (fruitLIst)

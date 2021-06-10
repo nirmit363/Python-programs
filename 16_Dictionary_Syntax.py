1087a4059b386475047171cfb32e269bb1604bc5
Nirmit Rath <nirmit363@gmail.com>
2021-06-10 17:36:18 +0530

    Add files via upload

---
+++ 16_Dictionary_Syntax.py
@@ -0,0 +1,8 @@
+My_Dict = {
+    'fast': 'In a quick Manner',
+    'Amresh': 'Hello! Amresh',
+    'marks': '[1,2,3]',
+    'secondDict': {'amresh': 'Hero'}
+}
+print (My_Dict['fast']+'\n'+My_Dict['Amresh']+'\n'+My_Dict['marks'])
+print (My_Dict['secondDict']['amresh'])

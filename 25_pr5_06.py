89f903e944df6224bc14ce09d8baf26997219521
Nirmit Rath <nirmit363@gmail.com>
2021-06-12 13:12:39 +0530

    Add files via upload

---
+++ 25_pr5_06.py
@@ -0,0 +1,12 @@
+favLang = {}
+a = input('Enter your favorite language Subham')
+b = input('Enter your favorite language Ankit')
+c = input('Enter your favorite language Sonali')
+d = input('Enter your favorite language Akash')
+
+favLang['Subham'] = a
+favLang['Ankita'] = b
+favLang['Sonali'] = c
+favLang['Akash'] = d
+
+print (favLang)

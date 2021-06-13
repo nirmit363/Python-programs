9f7fb14bc592f7e2a053816e73b6bb87724b06dc
Nirmit Rath <nirmit363@gmail.com>
2021-06-13 10:03:45 +0530

    Add files via upload

---
+++ 28_if_else_quiz.py
@@ -0,0 +1,8 @@
+age = int(input('Enter your Age\n'))
+
+if (age >= 18):
+    print ('Congrats! You are now an adult\n')
+else:
+    print ('Sorry! You are not an adult\n')
+
+print ('Thank you for using our software')

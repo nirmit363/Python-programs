87575d79a09534941d3bb6a618df8f8dca3ab4cd
Nirmit Rath <nirmit363@gmail.com>
2021-06-13 10:03:24 +0530

    Add files via upload

---
+++ 27_if_statement.py
@@ -0,0 +1,15 @@
+a = 66
+
+if (a >65):
+    print ('It is grater than 65')
+
+if (a > 4):
+    print ('It is grater than 4')
+
+if (a < 4):
+    print ('It is leass than 4')
+else:
+    print ("Don't know")
+
+
+print ('Done!')

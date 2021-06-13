d6bb5adadb0b40700bdaa2432100be41bf925bb7
Nirmit Rath <nirmit363@gmail.com>
2021-06-13 10:03:12 +0530

    Add files via upload

---
+++ 26_conditional.py
@@ -0,0 +1,10 @@
+a = 45
+
+if (a > 3):
+    print ('The value of a is greater than 3')
+elif(a > 7):
+    print ('The value of a is greater than 7')
+else:
+    print ('The alue is not greater than 3 or 7')
+
+print ('Done!')

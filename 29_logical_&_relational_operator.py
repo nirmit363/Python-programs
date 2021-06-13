b0ad060680c513ccc04e3ec3a926328c08f34370
Nirmit Rath <nirmit363@gmail.com>
2021-06-13 10:03:57 +0530

    Add files via upload

---
+++ 29_logical_&_relational_operator.py
@@ -0,0 +1,12 @@
+age = int(input('Enter your age\n'))
+
+if (age > 34 and age < 56):
+    print ('You can work with us.')
+else:
+    print ('You can not work with us.')
+
+age1 = int(input('Enter your age\n'))
+if (age1 > 34 or age1 < 56):
+    print ('You can work with us.')
+else:
+    print ('You can not work with us.')

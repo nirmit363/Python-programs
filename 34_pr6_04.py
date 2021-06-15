744f3057bdf566eda57e967a54bbd94277edef66
Nirmit Rath <nirmit363@gmail.com>
2021-06-15 18:35:56 +0530

    Add files via upload

---
+++ 34_pr6_04.py
@@ -0,0 +1,5 @@
+username = input('Enter user name:\n')
+if (len(username)<10):
+    print ('Yes! User name contains less than 10 characters')
+else:
+    print ('User name does not contain less than 10 characters')

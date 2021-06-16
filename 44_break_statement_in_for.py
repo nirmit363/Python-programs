b3c5679b90a2919cc273ce9f2796b89f5b9861a1
Nirmit Rath <nirmit363@gmail.com>
2021-06-16 20:17:32 +0530

    Add files via upload

---
+++ 44_break_statement_in_for.py
@@ -0,0 +1,7 @@
+for i in range(1, 11):
+    print (i)
+    if (i == 5):
+        break
+else:# Else will execute only after successful execution of for loop
+    print ('Finish')
+# Only execute if there is no break statement

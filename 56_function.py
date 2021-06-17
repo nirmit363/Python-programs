5c745d084951e417de4a035d795a16650a415d8e
Nirmit Rath <nirmit363@gmail.com>
2021-06-17 20:02:44 +0530

    Add files via upload

---
+++ 56_function.py
@@ -0,0 +1,9 @@
+def percent(marks):
+    return ((sum(marks)/400) * 100)
+
+marks = [45, 78, 86, 77]
+percentage = percent(marks)
+
+marks1 = [35, 76, 80, 77]
+percentage1 = (sum(marks1)/400) * 100
+print (percentage, percentage1 )

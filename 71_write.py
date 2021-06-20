adbba76d04ee9201dc8387b826a5e4e9e32c61b7
Nirmit Rath <nirmit363@gmail.com>
2021-06-20 18:34:22 +0530

    Add files via upload

---
+++ 71_write.py
@@ -0,0 +1,9 @@
+# f = open('71_another.txt', 'w')
+# f.write('Please write this to the file')
+# f.close()
+
+x = open('71_another.txt', 'w')
+x.write('I am writing .')
+x.write('I am Amresh.')
+x.write('Hello.')
+x.close

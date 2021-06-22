1970004cfd6f606ae6514d817f430ab5e0dc2d61
Nirmit Rath <nirmit363@gmail.com>
2021-06-22 20:44:04 +0530

    Update 88_self.py

--- 88_self.py
+++ 88_self.py
@@ -3,9 +3,9 @@ class Employee:
     def getSalary(self):
         print (f'Salary for {self.name} working in {self.company} is {self.salary}')
 
-amresh = Employee()
-amresh.name = 'Nirmit'
-amresh.salary = 100000
-amresh.getSalary()
-# Employee.getSalary(amresh)
-# actually amresh.getSalary() converts to Employee.getSalary(amresh) in machine point of view
+nirmit = Employee()
+nirmit.name = 'Nirmit'
+nirmit.salary = 100000
+nirmit.getSalary()
+# Employee.getSalary(nirmit)
+# actually nirmit.getSalary() converts to Employee.getSalary(nirmit) in machine point of view

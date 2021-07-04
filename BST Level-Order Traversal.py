03d83e16042e4d3ddba813f5dcd1ef750ce4b068
Nirmit Rath <nirmit363@gmail.com>
2021-07-04 10:39:49 +0530

    Add files via upload

---
+++ BST Level-Order Traversal.py
@@ -0,0 +1,16 @@
+
+
+    def levelOrder(self,root):
+        queue = [root]
+        while len(queue) is not 0:
+            curr = queue[0]
+            queue = queue[1:]
+            print(str(curr.data) + " ", end="")
+
+            if curr.left is not None:
+                queue.append(curr.left)
+            if curr.right is not None:
+                queue.append(curr.right)
+
+        #Write your code here
+

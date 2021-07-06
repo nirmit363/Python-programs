3453da83c064515bb34168985955cd8c30fe2107
Nirmit Rath <nirmit363@gmail.com>
2021-07-06 21:14:25 +0530

    Add files via upload

---
+++ Datatypes.py
@@ -0,0 +1,16 @@
+
+# Declare second integer, double, and String variables.
+i1 = None
+d1 = None
+s1 = None
+# Read and save an integer, double, and String to your variables.
+i1 = int(input())
+d1 = float(input())
+s1 = str(input())
+# Print the sum of both integer variables on a new line.
+print (i+i1)
+# Print the sum of the double variables on a new line.
+print (d+d1)
+print (s+s1)
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
